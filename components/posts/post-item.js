f4347d56e49a1ff554ab7984adf1df41d092049f
Raman Temre <ramantemre2110@gmail.com>
2024-06-10 17:21:42 +0530

    feat: Add post grid & items

---
+++ components/posts/post-item.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./post-item.module.css";
+function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+  const formattedDate = new Date(date).toLocaleDateString("en-us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imgaePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
+  return (
+    <li className={classes.post}>
+      <Link href={linkPath}>
+        <div className={classes.image}>
+          <Image src={imgaePath} alt={title} width={300} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
+}
+export default PostItem;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,46 @@
+import FeaturedPosts from "../components/home-page/featured-post";
 import Hero from "../components/home-page/hero";
 
 function HomePage(params) {
-  return <Hero />;
+  const DUMMY_POSTS = [
+    {
+      slug: "getting-started-01",
+      title: "Getting Started with Next.js",
+      image: "getting-started-01.png",
+      excerpt:
+        "Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.",
+      date: "2024-06-10",
+    },
+    {
+      slug: "getting-started-02",
+      title: "Getting Started with Next.js",
+      image: "getting-started-02.png",
+      excerpt:
+        "Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.",
+      date: "2024-06-10",
+    },
+    {
+      slug: "getting-started-03",
+      title: "Getting Started with Next.js",
+      image: "getting-started-03.png",
+      excerpt:
+        "Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.",
+      date: "2024-06-10",
+    },
+    {
+      slug: "getting-started-04",
+      title: "Getting Started with Next.js",
+      image: "getting-started-04.png",
+      excerpt:
+        "Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.",
+      date: "2024-06-10",
+    },
+  ];
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </>
+  );
 }
 export default HomePage;
