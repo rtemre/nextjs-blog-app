5fb915c5c104d754eea59e73e59c49a16eb8731c
Raman Temre <ramantemre2110@gmail.com>
2024-06-13 12:11:05 +0530

    Use React protal for notification in _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* If we wants to use the React portal then we can utlize this _document.js file  */}
+          {/* Now, In this app we are using React portal for notification in components/ui/notification.js file */}
+          <div id="notifications"></div>
         </body>
       </Html>
     );
